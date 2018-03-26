704fe5d2c104662cc5db4787df2b4fb173fcf498
Anyama Richard <richard@Anyamas-MacBook-Pro.local>
2018-03-26 14:28:11 +0300

    [Feature] Add Header, Footer and sytle files

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="row">
+        <footer className="py-4 w3-blue-grey bg-dark footer">
+          <div className="container">
+            <p className="m-0 text-center text-white small">&copy; {(new Date().getFullYear())}
+              , All Rights Reserved!
+            </p>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
