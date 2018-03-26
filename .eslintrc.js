4467f057bb061a89af1ea572d9296ee07ae96350
Anyama Richard <richard@Anyamas-MacBook-Pro.local>
2018-03-26 12:37:21 +0300

    [Chore #155703212] Add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules":{
+        "react/prefer-stateless-function":0,
+        "react/jsx-filename-extension":0,
+        "no-undef":0,
+        "jsx-a11y/href-no-hash":0,
+    }
+
+};
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Users</li>
+          <li>Categories</li>
+          <li>Recipes</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Header;
+
